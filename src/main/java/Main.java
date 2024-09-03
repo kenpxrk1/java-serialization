8051adfe475b380b9910ca0fdc58c62270934bf5
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-09-03 16:36:00 +0300

    refactor: namings, constants, imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,29 @@
-import java.io.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
 
+
 public class Main {
 
+    public static final String userFilePath = "src/main/resources/users";
+    public static final String usersFilePath = "src/main/resources/users";
+
     public static void main(String[] args) {
-        User user1 = new User(1, "user1", LocalDate.now());
-        User user2 = new User(2, "user2", LocalDate.now().minusDays(1));
-        User user3 = new User(3, "user3", LocalDate.now().minusDays(2));
-        User[] users = {user1, user2, user3};
-        serialize(users, "src/main/resources/users");
-        serialize(user1, "src/main/resources/user");
-        System.out.println(deserialize("src/main/resources/user"));
-        System.out.println(Arrays.toString(deserializeArray("src/main/resources/users")));
+        User firstUser = new User(1, "user1", LocalDate.now());
+        User secondUser = new User(2, "user2", LocalDate.now().minusDays(1));
+        User thirdUser = new User(3, "user3", LocalDate.now().minusDays(2));
+        User[] users = {firstUser, secondUser, thirdUser};
+        serialize(users, usersFilePath);
+        serialize(firstUser, userFilePath);
+        System.out.println(deserialize(userFilePath));
+        System.out.println(Arrays.toString(deserializeArray(usersFilePath)));
     }
 
     public static void serialize(User user, String filepath) {
